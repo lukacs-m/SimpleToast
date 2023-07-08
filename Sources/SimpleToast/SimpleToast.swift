fd4057098dc6e2746331fb7fb13dc3c849af33d2
Martin <mlukacs.dev@gmail.com>
2023-07-08 18:57:19 +0200

    Initial Commit

---
+++ Sources/SimpleToast/SimpleToast.swift
@@ -0,0 +1,6 @@
+public struct SimpleToast {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SimpleToastTests/SimpleToastTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SimpleToast
+
+final class SimpleToastTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SimpleToast().text, "Hello, World!")
+    }
+}
